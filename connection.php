8c643d78cb61f32187feb192ae1f359a15230405
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-11-10 21:01:55 +0700

    .

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-    $Connect = pg_connect("postgres://gmpcstrqfaogmy:d0a56b7680988fcc7e23d4ce63fa22ca94fa9747cb3c5a157cd4a4d47e74c621@ec2-35-168-65-132.compute-1.amazonaws.com:5432/dbdncl8acmh72r");
+    $conn = pg_connect("postgres://gmpcstrqfaogmy:d0a56b7680988fcc7e23d4ce63fa22ca94fa9747cb3c5a157cd4a4d47e74c621@ec2-35-168-65-132.compute-1.amazonaws.com:5432/dbdncl8acmh72r");
 	//$Connect =pg_connect("host=localhost port=5432 dbname=postgres pass=123456")
-	if (!$Connect) {
+	if (!$conn) {
         die("Connection failed");
     }
 ?>
