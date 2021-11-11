34e3e1b0f64ddba14a5e4ff97f2b2b5a6b6dfb67
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-11-11 10:26:24 +0700

    .

--- Search.php
+++ Search.php
@@ -1,33 +1,33 @@
 <div class="container">
-    <h1><u>Search Results.</u></h1>
+    <h1><u>All Product</u></h1>
 </div>
 <div class="container">
     <?php
-
-						              include_once("connection.php");
-                                      if (isset($_POST["txtSearch"])) {
-                                          $data = $_POST['txtSearch'];
-                                            if($data=="")
-                                            {
-                                              echo "<script>alert('Please Enter Data!')</script>";
-                                              echo '<meta http-equiv="refresh" content="0;URL=index.php">';
-                                            }
-                                           else {
-		  				   	        $result = pg_query($conn, "SELECT * FROM product where product_id like '%".$data."%' or product_name like '%".$data."%'");
-    			                if(pg_num_rows($result)==0)
-                          {
-                            echo  "<script>alert('No find data. Please Enter Again!')</script>";
-                            echo '<meta http-equiv="refresh" content="0;URL=index.php">';
+			 include_once("connection.php");
+          if (isset($_POST["txtSearch"])) {
+              $data = $_POST['txtSearch'];
+                if($data=="")
+                  {
+                  echo "<script>alert('Please Enter Data!')</script>";
+                  echo '<meta http-equiv="refresh" content="0;URL=index.php">';
+                  }
+                else {
+		  				   	$result = pg_query($conn, "SELECT * FROM product where product_id like '%$data%' or product_name like '%$data%'");
+    			          if(pg_num_rows($result)==0)
+                      {
+                        echo  "<script>alert('No find data. Please Enter Again!')</script>";
+                        echo '<meta http-equiv="refresh" content="0;URL=index.php">';
+                      }
+                    else {
+                      if (!$result) {
+                          die('Invalid query: ' . pg_errormessage($conn));
                           }
-                          else {
-                           if (!$result) { //add this check.
-                                die('Invalid query: ' . pg_errormessage($conn));
-                            }
-                            else {
-			                    while($row = pg_fetch_array($result, NULL, PGSQL_ASSOC)){
+                        else {
+			                  while($row = pg_fetch_array($result,NULL, PGSQL_ASSOC)){
 				                  ?>
     <!--Display product-->
-    <div class="product-f-imagemu">
+
+                                <div class="product-f-imagemu">
                                    <img src="img/<?php echo $row['pro_image']?>" width="550" height="450">
                                     <div class="product-hover">
                                         <a href="?page=product_management&&id=<?php echo  $row['pro_image']?>" class="view-details-link"><i class="fa fa-link"></i> View Details</a>
